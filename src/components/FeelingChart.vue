65778fc9cd4d1c41e52028510964dd413306a67d
jorge aguilera <jorge@edn.es>
2025-10-03 07:48:22 +0200

    dont use base64

--- src/components/FeelingChart.vue
+++ src/components/FeelingChart.vue
@@ -21,8 +21,23 @@ watch(modelValue, (value) => {
   max.value = [value.max, value.max, value.max, value.max];
 }, {deep: true});
 
+const convertDataUrlToBlob = (dataUrl) =>{
+  const arr = dataUrl.split(',');
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new Blob([u8arr], {type: mime});
+}
+
+
 const getChartImage = () => {
-  return chartController.value.chartInstance.toBase64Image();
+  return convertDataUrlToBlob( chartController.value.chartInstance.toBase64Image() )
 }
 
 const plugin = {
